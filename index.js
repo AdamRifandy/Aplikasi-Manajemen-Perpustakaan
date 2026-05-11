f6d6452cbd0a18a2413ec66e38d526e397dd29e3
AdamRifandy <adamrifandy123@gmail.com>
2026-05-11 09:35:16 +0700

    update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Running application on port: ${process.env.PORT}.`);
+});
