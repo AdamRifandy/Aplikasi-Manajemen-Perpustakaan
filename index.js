6b56e9fb740c65d4b4dca6a0efb860a96f5578e0
lioraAndy4027 <andycraftsaga4027@gmail.com>
2026-05-10 19:47:28 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import route from "./controllers/routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+app.use("/perpustakaan", route);
 
 app.listen(process.env.PORT, () => {
   console.log(`Running application on port: ${process.env.PORT}.`);
