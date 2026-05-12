e9040f3b8dccf58c833b05128763564626e29e01
Andy ft. Acumalaka <adamrifandy123@gmail.com>
2026-05-12 04:20:02 -0700

    update models/db.js

--- models/db.js
+++ models/db.js
@@ -2,7 +2,14 @@ import { DatabaseSync } from "node:sqlite";
 
 const conn = new DatabaseSync("data_perpustakaan.db");
 try {
-  conn.exec(``);
+  conn.exec(`
+    CREATE TABLE IF NOT EXISTS buku (
+      id_buku INTEGER PRIMARY KEY AUTOINCREMENT,
+      judul TEXT NOT NULL,
+      penulis TEXT NOT NULL,
+      status TEXT NOT NULL
+    )
+  `);
 } catch (err) {
   console.error(err);
 }
