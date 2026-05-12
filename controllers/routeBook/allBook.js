6a9f234e411892872c0f5da655c3d402a01682a4
Andy ft. Acumalaka <adamrifandy123@gmail.com>
2026-05-12 11:12:44 -0700

    update controllers/routeBook/allBook.js

--- controllers/routeBook/allBook.js
+++ controllers/routeBook/allBook.js
@@ -1,1 +1,19 @@
+import conn from "../../models/db.js";
 
+export default (req, res) => {
+  try {
+    const sql = conn.prepare("SELECT * FROM buku").all();
+    if (!sql) return res.status(500).json({
+      message: "Gagal mendapatkan semua data buku!",
+      data: sql,
+      status: false
+    });
+    return res.json({
+      message: "Berhasil mendapatkan semua data buku.",
+      data: sql,
+      status: true
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message, data: err, status: false });
+  }
+};
