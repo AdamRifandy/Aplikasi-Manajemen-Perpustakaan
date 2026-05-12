6e473a9f3870f1651e19050866fff8050b8bd8a5
Andy ft. Acumalaka <adamrifandy123@gmail.com>
2026-05-12 11:18:08 -0700

    update controllers/routes.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import allBook from "./routeBook/allBook.js";
 
 const route = Router();
-route.get("/all", allBook);
+route.get("/book/all", allBook);
 
 export default route;
